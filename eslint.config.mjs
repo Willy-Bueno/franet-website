838f378451aee9eee528e6e0759e160e300937df
Willy-Bueno <willybueno090@gmail.com>
2024-08-15 13:46:21 -0300

    chore: add eslint plugin tailwindcss

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-// @ts-check
+// @ts-ignore
+import tailwind from "eslint-plugin-tailwindcss";
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   // Your custom configs here
 )
+.append(...tailwind.configs["flat/recommended"])
