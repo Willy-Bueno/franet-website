86f57ef508d923b6c8c8f5b0dbe73ecc4b8610dc
Willy-Bueno <willybueno090@gmail.com>
2024-08-30 17:20:29 -0300

    refactor: ajusta os banners

--- components/page/franet-4g/banner.vue
+++ components/page/franet-4g/banner.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="aspect-[3/1] w-full">
-    <img class="w-full" src="/images/franet-movel.png" alt="Combo" >
+  <div class="aspect-square w-full sm:aspect-[3/1]">
+    <img class="hidden w-full sm:block" src="/images/franet-movel.png" alt="Combo" >
+    <img class="block w-full sm:hidden" src="/images/franet-movel-sm.png" alt="Combo" >
   </div>
 </template>
